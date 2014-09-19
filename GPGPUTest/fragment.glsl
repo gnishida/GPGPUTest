421fed50f7ffff0e7eefe9024c47a63a6cf4ee22
gnishida <gnishida@purdue.edu>
2014-09-18 20:23:09 -0400

    update.

--- GPGPUTest/fragment.glsl
+++ GPGPUTest/fragment.glsl
@@ -1,6 +1,7 @@
 uniform int initialized;
 uniform sampler2D tex;
 
+// Initialize the image
 void init()
 {
 	 vec2 texCoord = gl_TexCoord[0].xy;
@@ -11,16 +12,20 @@ void init()
 	 }
 }
 
+// update the color of each pixel
 void update()
 {
 	const float offset = 1.0 / 512.0;
 	vec2 texCoord = gl_TexCoord[0].xy;
 
+	// get the colors of the surrounding pixels
 	vec4 c  = texture2D(tex, texCoord);
 	vec4 l  = texture2D(tex, texCoord + vec2(-offset,     0.0));
 	vec4 t  = texture2D(tex, texCoord + vec2(    0.0,  offset));
 	vec4 r  = texture2D(tex, texCoord + vec2( offset,     0.0));
 	vec4 b  = texture2D(tex, texCoord + vec2(    0.0, -offset));
+
+	// use the heat equation to updte the color of this pixel
 	gl_FragColor = c + (l + t + r + b - c * 4.0) * 0.2;
 }
 
